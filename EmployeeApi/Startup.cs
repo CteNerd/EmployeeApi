9a96fe174991cf345d748d8cf5e08c79195e8456
rtomlin62 <rtomlin62@gmail.com>
2021-03-31 14:09:16 -0700

    expose swagger regardless of env

--- EmployeeApi/Startup.cs
+++ EmployeeApi/Startup.cs
@@ -94,12 +94,9 @@ namespace EmployeeApi
 
             _configuration = builder.Build();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmployeeApi v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmployeeApi v1"));
 
             // app.UseHttpsRedirection();
 
